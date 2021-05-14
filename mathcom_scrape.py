726feb1da94b664b4b5a21de6a458b86efdc04bb
amoebefan <62589341+amoebefan@users.noreply.github.com>
2021-05-14 22:32:22 +0200

    grab topics for subjects

--- mathcom_scrape.py
+++ mathcom_scrape.py
@@ -3,17 +3,25 @@ import requests
 from bs4 import BeautifulSoup
 
 mathcom_url = requests.get('http://www.math.com/')
-
 mathcom = BeautifulSoup(mathcom_url.content, 'html5lib')
-
 mathcom_url.close()
 
 subjects = {}
-
 for subject in mathcom.find_all('a',{'class':'nav_menu'}):
     href = subject.get('href')
     text = subject.get_text()
     if href.startswith('/homeworkhelp'):
-        subjects[text]=href
+        subjects[text] = href
 
-print(subjects)
+topics = {}
+for subject in subjects:
+    mathcom_subject_url = requests.get('http://www.math.com' + subjects[subject])
+    mathcom_subject = BeautifulSoup(mathcom_subject_url.content, 'html5lib')
+    mathcom_subject_url.close()
+    for topic in mathcom_subject.find_all('a'):
+        href = topic.get('href')
+        text = topic.get_text()
+        if href.startswith('/school/subject'):
+            topics[text] = href
+
+print(topics)

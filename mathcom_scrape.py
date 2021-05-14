39b8a1e2ebfa149eea7804b12a992540953943a8
amoebefan <62589341+amoebefan@users.noreply.github.com>
2021-05-14 21:47:31 +0200

    get subjects from homepage

--- mathcom_scrape.py
+++ mathcom_scrape.py
@@ -2,5 +2,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-mathcom = requests.get(http://www.math.com/)
+mathcom_url = requests.get('http://www.math.com/')
 
+mathcom = BeautifulSoup(mathcom_url.content, 'html5lib')
+
+mathcom_url.close()
+
+subjects = {}
+
+for subject in mathcom.find_all('a',{'class':'nav_menu'}):
+    href = subject.get('href')
+    text = subject.get_text()
+    if href.startswith('/homeworkhelp'):
+        subjects[text]=href
+
+print(subjects)

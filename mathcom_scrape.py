bcf1bd61cdd2d2eb82df4a1bf6a1e20685e3e9d0
amoebefan <62589341+amoebefan@users.noreply.github.com>
2021-05-24 12:55:41 +0200

    collect data for each page and write data in dict

--- mathcom_scrape.py
+++ mathcom_scrape.py
@@ -1,6 +1,7 @@
 #%%
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 mathcom_url = requests.get('http://www.math.com/')
 mathcom = BeautifulSoup(mathcom_url.content, 'html5lib')
@@ -13,16 +14,23 @@ for subject in mathcom.find_all('a',{'class':'nav_menu'}):
     if href.startswith('/homeworkhelp'):
         subjects[text] = href
 
-topics = {}
+data = []
 for subject in subjects:
     mathcom_subject_url = requests.get('http://www.math.com' + subjects[subject])
     mathcom_subject = BeautifulSoup(mathcom_subject_url.content, 'html5lib')
     mathcom_subject_url.close()
     for topic in mathcom_subject.find_all('a'):
+        topic_data = {}
         href = topic.get('href')
         text = topic.get_text()
         if href.startswith('/school/subject'):
             if href.endswith('Quiz.html'):
                 None
             else:
-                topics[text] = href
+                topic_data['subject'] = subject
+                topic_data['subsubject'] = 'subsubject'
+                topic_data['topic'] = text
+                topic_data['url'] = 'http://www.math.com' + subjects[subject] + href
+                data.append(topic_data)
+
+print(data)
